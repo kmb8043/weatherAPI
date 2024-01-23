96f33d16783b71eda6e3c5bac9bbe29349078a2c
kmb8043 <you@example.com>
2024-01-22 19:41:03 -0500

    worked on correcting js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var APIkey = '4ddbaa190fc883f9a34a66a689a819db';
+var APIkey = '5b756daa2c3f8347f2ab7b391a0b28b3';
 
 // city search bar //
 var cityInput = document.getElementById('city-input')
@@ -8,97 +8,82 @@ var searchbtn = document.getElementById('search-btn');
 var clearbtn = document.getElementById('clear-btn');
 var pastsearch = document.getElementById('prevSearches');
 
-
 // function to get the weather //
-function getWeather(){
-  var weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${APIkey}`;
-    fetch(weatherURL)
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(data){
-      console.log(data);
-
-    var currentWeather = document.getElementById('currentweather');
-      currentWeather.addClass('border border-primary');
-
-    var cityName = document.createElement('<h2>');
-      cityName.text('currentCity');
-      currentWeather.append(cityName);
-
-    var currentDate = data.current.dt;
-      currentDate = moment.unix(currentDate).format('MM/DD/YYYY');
-
-    var date = document.createElement('<div>');
-      date.text(`(${currentDate})`);
-      cityName.append(currentDate);
-
-    var cityTemp = data.current.temp;
-    var currentTemp = document.createElement('<div>');
-
-      currentTemp.text(`Temperature: ${cityTemp}`);
-      currentWeather.append(cityTemp);
-
-    var currentWind = data.current.wind_speed;
-    var windEl = document.createElement('<div>');
-
-      windEl.text(`Wind: ${currentWind}`);
-      currentWeather.append(windEl);
-
-    var currentHumidity = data.current.humidity;
-    var humidity = document.createElement('<div>');
-
-      humidity.text(`Humidity: ${currentHumidity} %`);
-      currentHumidity.append(humidity);
-
-// five day forecast //
-      var fiveDayHeader = document.getElementById('5dayheader');
-      var fiveDayEl = document.createElement('<h2>');
-      
-        fiveDayEl.text('5 Day Forecast:');
-        fiveDayHeader.append(fiveDayEl);
-
-        fiveDayForecast = document.getElementById('5dayforecast');
-        for (let i = 1; i <= 5; i++){
-          var day;
-          var temp;
-          var wind;
-          var humidity;
-
-          day = data.daily[i].dt;
-          day = moment.unix(date).format('MM/DD/YYYY');
-
-          temp = data.daily[i].temp.day;
-          wind = data.daily[i].wind.daily;
-          humidity = data.daily[i].humidity;
-
-    // cards for the 5 day forecast //
-          var card = document.createElement('<div>');
-          card.classList.add('card' , 'col-2' , 'm-1' , 'bg-primary' , 'text-white');
-
-          var cardBody = document.createElement('<div>');
-          cardBody.classList.add('card-body');
-          cardBody.innerHtml = `<div> ${day} <div>
-                                <div> ${temp} *F <div>
-                                <div> ${wind} <div>
-                                <div> ${humidity} % <div>`
-          card.appendChild(cardBody);
-          fiveDayForecast.append(card);
-        }
-
-    })
-    return;
-
-// function to get coordinates //
-    function getCoordinates (){
-      var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${APIkey}`;
+function getWeather() {
+    var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid= ${APIkey}`;
     
-      fetch(requestUrl)
-      .then(function(data){
-        
-      })
-    }
+    fetch(requestUrl)
+        .then(function(data) {
+            return data.json();
+        })
+        .then(function(geoData) {
+            var weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${geoData[0].lat}&lon=${geoData[0].lon}&exclude=minutely,hourly,alerts&units=metric&appid=${APIkey}`;
+            return fetch(weatherURL);
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
 
+            var currentWeather = document.getElementById('currentweather');
+            currentWeather.classList.add('border', 'border-primary');
+
+            var cityName = document.createElement('h2');
+            cityName.textContent = 'currentCity';
+            currentWeather.appendChild(cityName);
+
+            var currentDate = data.current.dt;
+            currentDate = moment.unix(currentDate).format('MM/DD/YYYY');
+
+            var date = document.createElement('div');
+            date.textContent = `(${currentDate})`;
+            cityName.appendChild(date);
+
+            var cityTemp = data.current.temp;
+            var currentTemp = document.createElement('div');
+            currentTemp.textContent = `Temperature: ${cityTemp}`;
+            currentWeather.appendChild(currentTemp);
+
+            var currentWind = data.current.wind_speed;
+            var windEl = document.createElement('div');
+            windEl.textContent = `Wind: ${currentWind}`;
+            currentWeather.appendChild(windEl);
+
+            var currentHumidity = data.current.humidity;
+            var humidity = document.createElement('div');
+            humidity.textContent = `Humidity: ${currentHumidity} %`;
+            currentWeather.appendChild(humidity);
+
+            // five day forecast //
+            var fiveDayHeader = document.getElementById('5dayheader');
+            var fiveDayEl = document.createElement('h2');
+            fiveDayEl.textContent = '5 Day Forecast:';
+            fiveDayHeader.appendChild(fiveDayEl);
+
+            var fiveDayForecast = document.getElementById('5dayforecast');
+            for (let i = 1; i <= 5; i++) {
+                var day = moment.unix(data.daily[i].dt).format('MM/DD/YYYY');
+                var temp = data.daily[i].temp.day;
+                var wind = data.daily[i].wind.daily;
+                var humidity = data.daily[i].humidity;
+
+                // cards for the 5 day forecast //
+                var card = document.createElement('div');
+                card.classList.add('card', 'col-2', 'm-1', 'bg-primary', 'text-white');
+
+                var cardBody = document.createElement('div');
+                cardBody.classList.add('card-body');
+                cardBody.innerHTML =
+                    `<div> ${day} </div>
+                    <div> ${temp} *F </div>
+                    <div> ${wind} </div>
+                    <div> ${humidity} % </div>`;
+
+                card.appendChild(cardBody);
+                fiveDayForecast.appendChild(card);
+            }
+        });
 }
 
-searchbtn.addEventListener('click' , getWeather);
+searchbtn.addEventListener('click', getWeather);
